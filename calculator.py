e8c7a81603671801c4dca8caa9ec348a2590d76e
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-14 21:23:42 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def cal(a,b,op):
+  if op == '+':
+    return a+b
+  elif op == '-':
+    return a-b
+  elif op == '*':
+    return a*b
+  elif == '/':
+     if b==0:
+       return "unable to divide by zero"
+     else:
+        return a/b
+  else:
+    return "invalid operator"
